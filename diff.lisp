8cacb681883d8f6c16b1d4a8c57958eb2e22237b
Peter Seibel <peter@gigamonkeys.com>
2010-11-23 12:45:46 -0800

    Unsaved change.

--- diff.lisp
+++ diff.lisp
@@ -74,17 +74,15 @@ Optionally frob the computed LCS before computing the diff."
 (defun split-positions (one-chunk parts)
   "Find the positions where one-chunk should be split to get pieces
   corresponding to the given parts, which are derived from one-chunk.
-  For instance, if one-chunk is a piece of text that was split up and
-  the pieces inserted at various places in the new document, it will
-  show up as a single deletion in the diff and multiple additions. Or
+  For instance, if one-chunk is a piece of text that was split and the
+  pieces inserted at various places in the new document, it will show
+  up in the diff as a single deletion and multiple additions. Or,
   conversely, if a bunch of separate pieces (from different
   paragraphs) in the original document were combined into contiguous
   text in the edited document, we would have a single addition and
   multiple deletions."
   (multiple-value-bind (one-chunk-lcs-indices combined-lcs-indices) 
       (lcs-positions one-chunk (concatenate-vectors parts))
-    (setf one-chunk-lcs-indices (coerce one-chunk-lcs-indices 'vector))
-    (setf combined-lcs-indices (coerce combined-lcs-indices 'vector))
     (loop for s in (part-starts parts)
        for prev-x = 0 then x
        for x = (position-if (lambda (x) (>= x s)) combined-lcs-indices :start prev-x)
