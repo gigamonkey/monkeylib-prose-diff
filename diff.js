dff7f4bcb49cfdb053851db12af37f91cc0354fc
Peter Seibel <peter@gigamonkeys.com>
2010-12-02 15:29:41 -0800

    Actually checking in Javascript changes and some more changes to CSS.

--- diff.js
+++ diff.js
@@ -1,33 +1,41 @@
 (function () {
-
-    function setup () {
-        $('#show_original').bind('click', showOriginal);
-        $('#show_new').bind('click', showNew);
-        $('#show_diff').bind('click', showDiff);
-        showDiff();
+    
+    function applyStyles (displayState, button) {
+        var styles = {
+            'original': {
+                'delete'     : '',
+                'add'        : 'hidden',
+                'moved-from' : '',
+                'moved-to'   : 'hidden'
+            },
+            
+            'new': {
+                'delete'     : 'hidden',
+                'add'        : '',
+                'moved-from' : 'hidden',
+                'moved-to'   : ''
+            },
+            
+            'diff': {
+                'delete'     : 'diff',
+                'add'        : 'diff',
+                'moved-from' : 'diff',
+                'moved-to'   : 'diff'
+            },
+        }[displayState];
+        
+        for (var style in styles) { 
+            $('.' + style).removeClass().addClass(style).addClass(styles[style]);
+        }
+        
+        $('.selected').toggleClass('selected');
+        $(button).toggleClass('selected');
     }
-
-    function showOriginal () {
-        $('.delete').removeClass().addClass('delete');
-        $('.add').removeClass().addClass('add hidden');
-        $('.moved-from').removeClass().addClass('moved-from');
-        $('.moved-to').removeClass().addClass('moved-to hidden');
-    }
-
-    function showNew () {
-        $('.delete').removeClass().addClass('delete hidden');
-        $('.add').removeClass().addClass('add');
-        $('.moved-from').removeClass().addClass('moved-from hidden');
-        $('.moved-to').removeClass().addClass('moved-to');
-    }
-
-    function showDiff () {
-        $('.delete').removeClass().addClass('delete delete-display');
-        $('.add').removeClass().addClass('add add-display');
-        $('.moved-from').removeClass().addClass('moved-from moved-from-display');
-        $('.moved-to').removeClass().addClass('moved-to moved-to-display');
-    }
-
-    $(document).ready(setup);
+    
+    $(document).ready(function () {
+        $('#show_diff').bind('click', function () { applyStyles('diff', this); }).click();
+        $('#show_original').bind('click', function () { applyStyles('original', this); });
+        $('#show_new').bind('click', function () { applyStyles('new', this); });
+    });
     
 })();
