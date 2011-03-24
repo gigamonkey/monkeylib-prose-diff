6241b5b35b5bd050637446333d1bbd48615d4bc1
Peter Seibel <peter@gigamonkeys.com>
2011-03-24 10:26:38 -0700

    Ignore comments when diffing.

--- html.lisp
+++ html.lisp
@@ -8,22 +8,69 @@
   (make-retagger 
    (mapcar (lambda (x) (cons x 'wrap-add-delete)) '(:add :delete :moved-to :moved-from))))
 
-(defun diff-to-html (original edited output)
-  (with-output-to-file (out output)
-    (render-sexps-to-stream
-     `(:body
-       ,@(diff-to-markup original edited)
-       ((:ul :id "buttons")
-        (:li ((:button :id "show_diff") "Diff"))
-        (:li ((:button :id "show_original") "Original"))
-        (:li ((:button :id "show_new") "New"))))
-     out
-     :stylesheets '("diff.css")
-     :scripts '("jquery-1.4.4.js" "diff.js")
-     :rewriter (compose 
-                (lambda (x) (footnotes :comments x))
-                (lambda (x) (footnotes :notes x))
-                *retagger*))))
+(defun diff-to-html (original-file edited-file output &key (css-dir "") (js-dir ""))
+  (let ((original (remove-comments (parse-file original-file :parse-links-p nil)))
+        (edited (remove-comments (parse-file edited-file :parse-links-p nil))))
+    (with-output-to-file (out output)
+      (render-sexps-to-stream
+       `(:body
+         ,@(diff-to-markup original edited)
+         ((:ul :id "buttons")
+          (:li ((:button :id "show_diff") "Diff"))
+          (:li ((:button :id "show_original") "Original"))
+          (:li ((:button :id "show_new") "New"))))
+       out
+       :stylesheets (loop for x in '("diff.css") collect (format nil "~a~a" css-dir x))
+       :scripts (loop for x in '("jquery-1.4.4.js" "diff.js") collect (format nil "~a~a" js-dir x))
+       :rewriter (compose  (lambda (x) (footnotes :note x)) *retagger*)))))
+
+(defun extract-comments (sexp)
+  (let ((num 0)
+        (comments ()))
+    (labels ((walker (x)
+               (cond
+                 ((stringp x) x)
+                 ((numberp x) x)
+                 ((symbolp x) x)
+                 ((eql (car x) :comment)
+                  (push x comments)
+                  `(:commentref ,(princ-to-string (incf num))))
+                 (t  `(,(car x) ,@(mapcar #'walker (cdr x)))))))
+      (let ((walked (walker sexp)))
+        (values walked (nreverse comments))))))
+
+(defun remove-comments (sexp)
+  (labels ((walker (x)
+             (cond
+               ((stringp x) (list x))
+               ((numberp x) (list x))
+               ((symbolp x) (list x))
+               ((eql (car x) :comment) nil)
+               (t
+                (let ((body (mapcan #'walker (cdr x))))
+                  (if body `((,(car x) ,@body)) nil))))))
+
+    (first (walker sexp))))
+
+(defun commentref->html (sexp)
+  (destructuring-bind (tag string) sexp
+    (assert (eql tag :commentref))
+    (let ((num (parse-integer string)))
+      `((:span :href (:format "~(#comment_~d~)" ,num)
+               :id (:format "commentref_~d" ,num)
+               :class "comment_ref")
+        (:character 9997)))))
+
+
+(defun comments->html (sexp)
+  (destructuring-bind (tag &rest comments) sexp
+    (assert (eql tag :comments))
+    `(:progn ,@(loop for comment in comments
+                  for num from 1 
+                  collect 
+                    (destructuring-bind (tag &rest body) comment
+                      (assert (eql tag :comment))
+                      `((:div :id (:format "~(~a_~d~)" ,tag ,num) :class "comment") ,@body))))))
 
 (defun wrap-add-delete (sexp)
   (destructuring-bind (which &rest wrapped) sexp
