bf3189e0ae0e24ad21f7eeecaa5a9bea88fa4b3a
Peter Seibel <peter@gigamonkeys.com>
2012-01-16 16:08:21 -0800

    Removing macro-utilities dependency.

--- packages.lisp
+++ packages.lisp
@@ -6,13 +6,11 @@
 
 (defpackage :com.gigamonkeys.prose-diff
   (:use :common-lisp
-	:cl-ppcre
-	:com.gigamonkeys.pathnames
-	:com.gigamonkeys.utilities
-	:com.gigamonkeys.macro-utilities
+        :cl-ppcre
+        :com.gigamonkeys.pathnames
+        :com.gigamonkeys.utilities
         :com.gigamonkeys.markup
         :com.gigamonkeys.markup.html)
   (:import-from :alexandria :compose)
   (:export
    :show-cuts))
-
